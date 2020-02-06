f7a4b4c17e551cc95267fa78a68c8faaf93d1983
Sarvendra Kumar Pankaj <60646141+skpankaj@users.noreply.github.com>
2020-02-06 10:43:38 +0530

    Update swap.cpp

--- swap.cpp
+++ swap.cpp
@@ -4,9 +4,9 @@ int main()
 {  
 int a=5, b=10;      
 cout<<"Before swap a= "<<a<<" b= "<<b<<endl;      
-a=a*b; //a=50 (5*10)    
-b=a/b; //b=5 (50/10)    
-a=a/b; //a=10 (50/5)    
+a=a*b; //a=60 (5*10)    
+b=a/b; //b=5 (60/10)    
+a=a/b; //a=10 (60/5)    
 cout<<"After swap a= "<<a<<" b= "<<b<<endl;      
 return 0;  
 }  
