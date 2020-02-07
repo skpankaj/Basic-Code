d90b031e1092996b690d29898aef1da8a6a332c9
Sarvendra Kumar Pankaj <60646141+skpankaj@users.noreply.github.com>
2020-02-07 14:51:31 +0530

    Update TRC3.C

--- TRC3.C
+++ TRC3.C
@@ -5,8 +5,8 @@ void main()
 int age;
 char sex;
 clrscr();
-printf("\t\t***check the person qualified for insurance***");
-printf("\n\n enter the age:");
+printf("\t\t***check the person Qualified for Insurance***");
+printf("\n\n Enter the Age:");
 scanf("%d",&age);
 fflush(stdin);
 printf("\n\n enter the sex (m/f)?");
@@ -14,12 +14,12 @@ scanf("%c",&sex);
 
 if((sex=='m')&&(age>=30)||(sex=='f'))
 {
-printf("\n\n   qualified for insurance");
+printf("\n\n   Qualified for Insurance");
 }
 else
 {
 printf("\n\n   not insured");
 }
-printf("\n\n\t\***thanks for using this programme*** ") ;
+printf("\n\n\t\***Thanks for using this Programme*** ") ;
 getch();
 }
